739425938bffdc025eaf66beb9a528f82b24d801
Lacalasha <lac234774@gmail.com>
2025-02-22 22:51:43 +0400

    feature
    sadsa

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-"use strict";
+let numbers = [2, 4, 6, 8, 10];
+let doubledNumbers = [];
+
+numbers.forEach(function (singleNum) {
+  doubledNumbers.push(singleNum * 2);
+});
+
+console.log(doubledNumbers);
